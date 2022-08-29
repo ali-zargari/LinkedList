000f852942955119c1dfdd062513905dd6abbc47
ali-zargari <77089778+ali-zargari@users.noreply.github.com>
2022-08-28 19:14:54 -0700

    Insert method implemented (untested)
    Testing directory added with a tester app.

--- src/SortedLinkedList.java
+++ src/SortedLinkedList.java
@@ -36,9 +36,7 @@ public class SortedLinkedList {
 	 * DO NOT MODIFY
 	 * Returns whether or not a list is empty
 	 */
-	public boolean isEmpty() {
-		return head==null && tail==null;
-	}
+	public boolean isEmpty() { return head==null && tail==null; }
 	
 	/*
 	 * DO NOT MODIFY
@@ -82,7 +80,49 @@ public class SortedLinkedList {
 	 */
 	public void insert(int i) {
 		Node toInsert = new Node(i);
-		
+
+		//----case where i is invalid----
+		if(toInsert == null) return;
+
+		//----case where list is empty----
+		if(isEmpty()){
+			head = toInsert;
+			return;
+		}
+
+		//----cases where list has items----
+
+		//if item is less than or equal to head.
+		if(toInsert.data <= head.data){
+			toInsert.next = head;
+			head.prev = toInsert;
+			head = toInsert;
+			return;
+		}
+
+		//if item is greater than or equal to tail.
+		if(toInsert.data >= tail.data){
+			tail.next = toInsert;
+			toInsert.prev = tail;
+			tail = toInsert;
+			return;
+		}
+
+		//item belongs somewhere in the middle of a non-empty list
+
+		Node temp = head.next;
+		while(temp != null){
+
+			if(toInsert.data >= temp.data
+			&& toInsert.data <= temp.next.data){
+				toInsert.next = temp.next;
+				temp.next.prev = toInsert;
+				temp.next = toInsert;
+				toInsert.prev = temp;
+				break;
+			}
+			temp = temp.next;
+		}
 		
 	}
 	
